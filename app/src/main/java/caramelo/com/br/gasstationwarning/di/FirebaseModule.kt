b49175fc223b28ddcc317cfeec0a6d335d8f6402
Lucas Caramelo <lucascaramelo@gmail.com>
2018-06-07 23:54:44 -0300

    add FirebaseFirestore Settings

--- app/src/main/java/caramelo/com/br/gasstationwarning/di/FirebaseModule.kt
+++ app/src/main/java/caramelo/com/br/gasstationwarning/di/FirebaseModule.kt
@@ -3,6 +3,7 @@ package caramelo.com.br.gasstationwarning.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
@@ -11,7 +12,17 @@ import org.kodein.di.generic.singleton
 
 val firebaseModule = Kodein.Module {
     bind<FirebaseAuth>() with singleton { FirebaseAuth.getInstance() }
-    bind<FirebaseFirestore>() with singleton { FirebaseFirestore.getInstance() }
+    bind<FirebaseFirestoreSettings>() with singleton {
+        FirebaseFirestoreSettings.Builder()
+            .setTimestampsInSnapshotsEnabled(true)
+            .setPersistenceEnabled(true)
+            .build()
+    }
+    bind<FirebaseFirestore>() with singleton {
+        val db = FirebaseFirestore.getInstance()
+        db.firestoreSettings = instance()
+        return@singleton db
+    }
     bind<CollectionReference>("stations_collection") with provider {
         instance<FirebaseFirestore>().collection("stations")
     }
